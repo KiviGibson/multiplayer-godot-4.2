c6b551878f29ff76435ff7d2cfb54b0584238618
KiviGibson <77674694+KiviGibson@users.noreply.github.com>
2024-08-25 21:40:37 +0200

    change speed to be a variable

--- both/game/unit/MovingUnit.gd
+++ both/game/unit/MovingUnit.gd
@@ -2,15 +2,16 @@ class_name MovingUnit
 
 extends Unit
 
-const SPEED = 5.0
+@export var speed = 5.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @export var pathfinding: NavigationAgent3D
+
 func _ready() -> void:
 	self.new_position.connect(_new_position)
+
 func _new_position() -> void:
 	pathfinding.target_position = target_position
+
 func _physics_process(_delta: float) -> void:
 	if not multiplayer.is_server():
 		return
@@ -18,10 +19,10 @@ func _physics_process(_delta: float) -> void:
 	if not pathfinding.is_navigation_finished():
 		direction = (pathfinding.get_next_path_position()-self.global_position).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 	
 	move_and_slide()
